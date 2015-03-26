9bb3b87142a3d58fc6512c764442d0644c66cc8f
marindraganov <marodraganov@gmail.com>
2015-03-26 17:44:24 +0200

    Start implementing player movement.

--- BoardGameWithoutName/GameLogic/Game/Game.cs
+++ BoardGameWithoutName/GameLogic/Game/Game.cs
@@ -22,5 +22,46 @@
         // public virtual CalculateRollScore(IEnumerable<IRollScoreRouls<int>> results)
         // {
         // }
+        public Game(string[] playersNames, Map map, GameSettings settings)
+        {
+            if (playersNames.Count() < 2 || playersNames.Count() > 6)
+            {
+                throw new ArgumentException("The number of players must be between 2 and 6!");
+            }
+
+            this.InitializePlayers(playersNames, this.Players, map.Start);
+
+            this.CurrPlayer = this.Players[0];
+            this.Dice = Dice.Instance;
+        }
+
+        public List<Player> Players { get; private set; }
+
+        // the player who's turn is now
+        public Player CurrPlayer { get; private set; }
+
+        public Dice Dice { get; private set; }
+
+        public void MoveCurrPlayer(Field targetField)
+        {
+            if (Dice.Value == 0 ||
+                !Map.FieldCanBeReached(this.CurrPlayer.Field, targetField, this.Dice.Value))
+            {
+                return;
+            }
+            else
+            {
+                this.CurrPlayer.MoveTo(targetField);
+                this.Dice.Clear();
+            }
+        }
+
+        private void InitializePlayers(string[] playersNames, List<Player> players, Field start)
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                players.Add(new Player(playersNames[i], start));
+            }
+        }
     }
 }
