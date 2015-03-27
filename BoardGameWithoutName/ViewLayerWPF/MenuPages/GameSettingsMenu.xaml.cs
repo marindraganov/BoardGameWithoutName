2324de7de825da506772688b3dcbdd94497aaf4c
marindraganov <marodraganov@gmail.com>
2015-03-27 23:54:12 +0200

    "New Game Menu" UI updated

---
+++ BoardGameWithoutName/ViewLayerWPF/MenuPages/GameSettingsMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace ViewLayerWPF.MenuPages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for GameSettingsMenu.xaml
+    /// </summary>
+    public partial class GameSettingsMenu : Page
+    {
+        public GameSettingsMenu()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackToMainBtnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Window.MainWindowFrame.Source = new Uri("ManuPages/MainMenu.xaml", UriKind.Relative);
+        }
+    }
+}
---
+++ BoardGameWithoutName/ViewLayerWPF/MenuPages/MainMenu.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace ViewLayerWPF.MenuPages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NewGameBtnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Window.MainWindowFrame.Source = new Uri("MenuPages/NewGameMenu.xaml", UriKind.Relative);
+        }
+
+        private void SettingsBtnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Window.MainWindowFrame.Source = new Uri("MenuPages/GameSettingsMenu.xaml", UriKind.Relative);
+        }
+
+        private void QuitBtnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ResumeGameBtnClick(object sender, RoutedEventArgs e)
+        {
+            GameWindow.Window.Show();
+            MainWindow.Window.Hide();
+        }
+
+        private void SaveBtnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Save Button Do Not Work :)");
+        }
+    }
+}
