831410f1737c4afbd22435793b121a819464cbbb
marindraganov <marodraganov@gmail.com>
2015-03-27 23:55:21 +0200

    Empty Field added

---
+++ BoardGameWithoutName/GameLogic/Map/Fields/EmptyField.cs
@@ -0,0 +1,18 @@
+﻿namespace GameLogic.Map.Fields
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // this field do not offer any actions to players
+    class EmptyField : Field
+    {
+        public EmptyField(string name, Color color, int row, int col)
+            : base(name, color, row, col)
+        {
+        }
+    }
+}
