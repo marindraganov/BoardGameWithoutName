fe2330238eedfb28d7501e82c513fb05a18686ed
marindraganov <marodraganov@gmail.com>
2015-04-08 06:28:14 +0300

    small changes

--- BoardGameWithoutName/GameLogic/Disasters/Virus.cs
+++ BoardGameWithoutName/GameLogic/Disasters/Virus.cs
@@ -1,10 +1,6 @@
 ﻿namespace GameLogic.Disasters
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     using GameLogic.Map;
 
