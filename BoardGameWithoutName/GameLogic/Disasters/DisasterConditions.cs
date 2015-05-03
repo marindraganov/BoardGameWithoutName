f862c686170a577386b5b2e72d5146fc5cf229b2
marindraganov <marodraganov@gmail.com>
2015-05-04 02:03:24 +0300

    DisasterGenerator.cs added

---
+++ BoardGameWithoutName/GameLogic/Disasters/DisasterConditions.cs
@@ -0,0 +1,18 @@
+﻿namespace GameLogic.Disasters
+{
+    public class DisasterConditions
+    {
+        internal DisasterConditions()
+        {
+            this.ChanceForAssault = 0;
+            this.ChanceForVirus = 0;
+            this.ChanceForEarthquake = 0;
+        }
+
+        public int ChanceForAssault { get; internal set; }
+
+        public int ChanceForVirus { get; internal set; }
+
+        public int ChanceForEarthquake { get; internal set; }
+    }
+}
---
+++ BoardGameWithoutName/GameLogic/Disasters/DisasterGenerator.cs
@@ -0,0 +1,25 @@
+﻿namespace GameLogic.Disasters
+{   
+    using System;
+
+    using GameLogic.Map;
+
+    internal class DisasterGenerator
+    {
+        private Random rnd = new Random();
+        private GameMap map;
+
+        internal DisasterGenerator(GameMap map)
+        {
+            this.map = map;
+            this.Conditions = new DisasterConditions();
+        }
+
+        public DisasterConditions Conditions { get; private set; }
+
+        private void SetConditions()
+        {
+
+        }
+    }
+}
