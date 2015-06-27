e8be4dae151dda7ebefb828c9a84ddfc6ddbed73
marindraganov <marodraganov@gmail.com>
2015-06-27 16:41:49 +0300

    Value convertors changes

--- BoardGameWithoutName/ViewLayerWPF/ValueConverters/BoolToVisibility.cs
+++ BoardGameWithoutName/ViewLayerWPF/ValueConverters/BoolToVisibility.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace ViewLayerWPF.ValueConverters
 {
-    class BoolToOpacity : IValueConverter
+    class BoolToVisibility : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            bool visible = (bool)value;
+            bool isTrue = (bool)value;
 
-            if (visible)
+            if (isTrue)
             {
-                return 0.20;
+                return Visibility.Visible;
             }
             else
             {
-                return 0;
+                return Visibility.Hidden;
             }
         }
 
